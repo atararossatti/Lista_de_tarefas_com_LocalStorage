60e615035a3b3e96ee2a0e789c24ed733b0ecb9d
Atara Rossatti <atarasilva@hotmail.com>
2022-06-16 00:04:12 -0300

    realizado_alteração_de_logo_cor_visual

---
+++ lista_tarefas_2022/projeto_inicial/componentes/main.js
@@ -0,0 +1,8 @@
+import {handleNovoItem} from '../componentes/criaTarefa.js'
+import {carregaTarefa} from '../componentes/carregaTarefa.js'
+
+const novaTarefa = document.querySelector ('[data-form-button]')
+
+novaTarefa.addEventListener('click', handleNovoItem)
+
+carregaTarefa ()
